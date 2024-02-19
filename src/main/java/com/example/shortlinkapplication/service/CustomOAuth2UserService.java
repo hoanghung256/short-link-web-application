3ff5a5576d64aa30858095c0b3810ea4c750b635
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-02-19 13:26:14 +0700

    add custom oauth2 userservice

---
+++ src/main/java/com/example/shortlinkapplication/service/CustomOAuth2UserService.java
@@ -0,0 +1,41 @@
+package com.example.shortlinkapplication.service;
+
+import com.example.shortlinkapplication.entity.User;
+import com.example.shortlinkapplication.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // validate if user is null
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String firstName = oAuth2User.getAttribute("given_name");
+        String lastName = oAuth2User.getAttribute("family_name");
+
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            user = new User();
+            user.setEmail(email);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            userRepository.save(user);
+        }
+        return new DefaultOAuth2User(Collections.emptySet(), oAuth2User.getAttributes(), "email");
+        }
+}
