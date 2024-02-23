6beaf73337b629ccbea4520abd11ebc79d1e359a
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-02-23 16:49:32 +0700

    validate email format

---
+++ src/main/java/com/example/shortlinkapplication/service/EmailValidator.java
@@ -0,0 +1,19 @@
+package com.example.shortlinkapplication.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String email) {
+        // regex for RFC-5322 validation
+        String regex = "^[a-zA-Z0-9_!#$%&amp;'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
