311aa2fd1c1c8375c4135cce17fe57eef42b37dd
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-02-23 16:53:08 +0700

    load email from db

---
+++ src/main/java/com/example/shortlinkapplication/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.shortlinkapplication.service;
+
+import com.example.shortlinkapplication.entity.User;
+import com.example.shortlinkapplication.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
