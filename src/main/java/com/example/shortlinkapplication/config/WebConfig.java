8dc7443db4627a84e52f015f6b7ca24d862d13a9
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-02-29 07:53:42 +0700

    config cors

--- src/main/java/com/example/shortlinkapplication/config/WebConfig.java
+++ src/main/java/com/example/shortlinkapplication/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.shortlinkapplication.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
     public void addCorsMappingS(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
