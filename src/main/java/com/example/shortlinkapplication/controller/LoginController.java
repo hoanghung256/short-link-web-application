78410955e7540449a9cb4a8fcfb65aea2f1b0166
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-03-11 11:53:29 +0700

    add logout get mapping

--- src/main/java/com/example/shortlinkapplication/controller/LoginController.java
+++ src/main/java/com/example/shortlinkapplication/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.example.shortlinkapplication.controller;
 
-import com.example.shortlinkapplication.dto.*;
+import com.example.shortlinkapplication.dto.ApiResponse;
+import com.example.shortlinkapplication.dto.LoginRequest;
+import com.example.shortlinkapplication.dto.LoginResponse;
+import com.example.shortlinkapplication.dto.SignUpRequest;
 import com.example.shortlinkapplication.entity.AuthProvider;
 import com.example.shortlinkapplication.entity.User;
 import com.example.shortlinkapplication.repository.UserRepository;
@@ -10,115 +13,103 @@ import com.example.shortlinkapplication.security.oauth.OAuth2AuthenticationFailu
 import com.example.shortlinkapplication.security.oauth.OAuth2AuthenticationSuccessHandler;
 import com.example.shortlinkapplication.service.LoginService;
 import com.example.shortlinkapplication.service.UserService;
-import com.example.shortlinkapplication.util.CookieUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.servlet.view.RedirectView;
-
-
-import java.io.IOException;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class LoginController {
-    @Autowired
-    private LoginService loginService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private UserRepository userRepository;
+  private final LoginService loginService;
+  private final AuthenticationManager authenticationManager;
+  private final UserRepository userRepository;
+  private final TokenProvider tokenProvider;
+  private final UserService userService;
+  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+  private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Autowired
-    private TokenProvider tokenProvider;
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    return ResponseEntity.ok(loginService.signin(loginRequest));
+  }
 
-    @Autowired
-    private UserService userService;
+  @GetMapping("/logout")
+  public ResponseEntity<?> logout() {
+    SecurityContextHolder.clearContext();
+    return ResponseEntity.ok("Logout successful");
+  }
 
-    @Autowired
-    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+  // if token is confirmed will create new jwt after
+  @GetMapping("/confirm")
+  public ResponseEntity<LoginResponse> confirm(@RequestParam("token") String token,
+      HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    Integer userID = loginService.confirmToken(token, request, response);
 
-    @Autowired
-    private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    // create new user authenticated
+    UserDetails userDetails = userService.loadUserById(userID);
+    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+        userDetails, null, userDetails.getAuthorities());
+    authentication.setDetails((new WebAuthenticationDetailsSource().buildDetails(request)));
 
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    SecurityContextHolder.getContext().setAuthentication(authentication);
 
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login (@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.ok(loginService.signin(loginRequest));
+    // call jwt authentication filter
+    jwtAuthenticationFilter.successfulAuthentication(request, response, null, authentication);
+
+    // get jwt token
+    User jwt = userRepository.findTokenByUserID(userID);
+    LoginResponse loginResponse = new LoginResponse(jwt.getToken());
+
+    return ResponseEntity.ok(loginResponse);
+
+  }
+
+  @GetMapping("/error")
+  public String handleError(@RequestParam("message") String message) {
+    return "Error: " + message;
+  }
+
+  @PostMapping("/signin")
+  public ResponseEntity<?> registerUser(@RequestBody SignUpRequest signUpRequest)
+      throws BadRequestException {
+    if (Boolean.TRUE.equals(userRepository.existsByEmail(signUpRequest.getEmail()))) {
+      throw new BadRequestException("Email address already in use.");
     }
 
-    // if token is confirmed will create new jwt after
-    @GetMapping("/confirm")
-    public ResponseEntity<LoginResponse> confirm(@RequestParam("token") String token, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        try {
-            Integer userID = loginService.confirmToken(token, request, response);
-            System.out.println("User: " + userID);
+    // Creating user's account
+    User user = new User();
+    user.setName(signUpRequest.getName());
+    user.setEmail(signUpRequest.getEmail());
+    user.setAuthProvider(AuthProvider.local);
 
-            // create new user authenticated
-            UserDetails userDetails = userService.loadUserById(userID);
-            System.out.println("UserDetails: " + userDetails);
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authentication.setDetails((new WebAuthenticationDetailsSource().buildDetails(request)));
+    User result = userRepository.save(user);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+    URI location = ServletUriComponentsBuilder
+        .fromCurrentContextPath().path("/user/me")
+        .buildAndExpand(result.getUserID()).toUri();
 
-            // call jwt authentication filter
-            jwtAuthenticationFilter.successfulAuthentication(request,response,null, authentication);
-
-            // get jwt token
-            User jwt = userRepository.findTokenByUserID(userID);
-            System.out.println("JWT: " + jwt.getToken());
-
-            LoginResponse loginResponse = new LoginResponse(jwt.getToken());
-            System.out.println("login response: " + loginResponse);
-
-            return ResponseEntity.ok(loginResponse);
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-            throw e;
-        }
-
-    }
-
-    @GetMapping("/error")
-    public String handleError (@RequestParam("message") String message) {
-       return "Error: " + message;
-    }
-    @PostMapping("/signin")
-    public ResponseEntity<?> registerUser( @RequestBody SignUpRequest signUpRequest) throws BadRequestException {
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
-            throw new BadRequestException("Email address already in use.");
-        }
-
-        // Creating user's account
-        User user = new User();
-        user.setName(signUpRequest.getName());
-        user.setEmail(signUpRequest.getEmail());
-        user.setAuthProvider(AuthProvider.local);
-
-        User result = userRepository.save(user);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentContextPath().path("/user/me")
-                .buildAndExpand(result.getUserID()).toUri();
-
-        return ResponseEntity.created(location)
-                .body(new ApiResponse(true, "User registered successfully@"));
-    }
+    return ResponseEntity.created(location)
+        .body(new ApiResponse(true, "User registered successfully@"));
+  }
 }
