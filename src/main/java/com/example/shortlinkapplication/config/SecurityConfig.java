631de8fe94be3344399899485b0edc4868293d4a
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-03-11 11:25:39 +0700

    update format code

--- src/main/java/com/example/shortlinkapplication/config/SecurityConfig.java
+++ src/main/java/com/example/shortlinkapplication/config/SecurityConfig.java
@@ -2,12 +2,13 @@ package com.example.shortlinkapplication.config;
 
 import com.example.shortlinkapplication.repository.UserRepository;
 import com.example.shortlinkapplication.security.TokenAuthenticationFilter;
+import com.example.shortlinkapplication.security.oauth.CustomOAuth2UserService;
 import com.example.shortlinkapplication.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.example.shortlinkapplication.security.oauth.OAuth2AuthenticationFailureHandler;
 import com.example.shortlinkapplication.security.oauth.OAuth2AuthenticationSuccessHandler;
 import com.example.shortlinkapplication.service.UserService;
-import com.example.shortlinkapplication.security.oauth.CustomOAuth2UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Collections;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -21,25 +22,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Collections;
-
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
-  @Autowired
-  private CustomOAuth2UserService oAuth2UserService;
-  @Autowired
-  private UserService userService;
-  @Autowired
+  private final CustomOAuth2UserService oAuth2UserService;
+  private final UserService userService;
   @Lazy
-  private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-  @Autowired
+  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
   @Lazy
-  private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-
-  @Autowired
-  private UserRepository userRepository;
+  private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+  private final UserRepository userRepository;
 
   @Bean
   public TokenAuthenticationFilter tokenAuthenticationFilter() {
@@ -59,13 +53,12 @@ public class SecurityConfig {
   }
 
   @Bean
-  public AuthenticationManager authenticationManager() throws Exception {
+  public AuthenticationManager authenticationManager() {
     return new ProviderManager(Collections.singletonList(authenticationProvider()));
   }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    System.out.println("Start filer chain");
     http
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
@@ -86,7 +79,6 @@ public class SecurityConfig {
         )
         .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-    System.out.println("Finish");
     return http.build();
   }
 
