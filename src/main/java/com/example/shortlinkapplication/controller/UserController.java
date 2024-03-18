fdb064d3f6f771e7773f3c0b0a3d7dca9fbc161f
ashleynguyen07 <nguyentramy.se@gmail.com>
2024-03-18 11:34:53 +0700

    commit

--- src/main/java/com/example/shortlinkapplication/controller/UserController.java
+++ src/main/java/com/example/shortlinkapplication/controller/UserController.java
@@ -4,27 +4,22 @@ import com.example.shortlinkapplication.entity.User;
 import com.example.shortlinkapplication.repository.UserRepository;
 import com.example.shortlinkapplication.security.CurrentUser;
 import com.example.shortlinkapplication.security.UserPrincipal;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @CrossOrigin
-    @GetMapping("user/me")
-    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-        System.out.println("getting current user");
-        return userRepository.findById(userPrincipal.getId()).orElseThrow(() -> new RuntimeException(String.valueOf(userPrincipal.getId())));
-    }
+  @CrossOrigin
+  @GetMapping("user/me")
+  public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+    return userRepository.findById(userPrincipal.getId())
+        .orElseThrow(() -> new RuntimeException(String.valueOf(userPrincipal.getId())));
+  }
 
 }
