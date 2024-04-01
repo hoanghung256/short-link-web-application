5063443c85da2c35a4d6baa2d675095c7590f6b1
Nguyễn Thị Trà My <89174992+ashleynguyen07@users.noreply.github.com>
2024-04-01 20:50:03 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/shortlinkapplication/config/SecurityConfig.java
+++ src/main/java/com/example/shortlinkapplication/config/SecurityConfig.java
@@ -36,55 +36,6 @@ public class SecurityConfig {
   @Autowired
   @Lazy
   private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-  @Autowired
-  private UserRepository userRepository;
-
-
-  @Bean
-  public TokenAuthenticationFilter tokenAuthenticationFilter() {
-    return new TokenAuthenticationFilter();
-  }
-
-  @Bean
-  public HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository() {
-    return new HttpCookieOAuth2AuthorizationRequestRepository();
-  }
-
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-    provider.setUserDetailsService(userService);
-    return provider;
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager() {
-    return new ProviderManager(Collections.singletonList(authenticationProvider()));
-  }
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(AbstractHttpConfigurer::disable)
-        .formLogin(AbstractHttpConfigurer::disable)
-        .sessionManagement(
-            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(requests -> requests
-            .requestMatchers("/auth/**", "/oauth2/**", "/signin", "/dashboard/**", "/public/**",
-                "/**", "/profile/**")
-            .permitAll()
-            .anyRequest().authenticated())
-        .oauth2Login(oauth -> oauth
-            .authorizationEndpoint(auth -> auth.baseUri("/oauth2/authorize")
-                .authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository())
-            )
-            .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
-            .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
-            .successHandler(oAuth2AuthenticationSuccessHandler)
-            .failureHandler(oAuth2AuthenticationFailureHandler)
-        )
-        .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
 
   @Autowired
   private UserRepository userRepository;
